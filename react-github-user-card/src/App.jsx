47a80c7c90260a4922b8f850584a612d4445ccd8
Westley Feller <feller.westley@gmail.com>
2019-11-02 09:50:28 -0700

    Added header

--- react-github-user-card/src/App.jsx
+++ react-github-user-card/src/App.jsx
@@ -2,12 +2,14 @@ import React, { StrictMode } from 'react';
 import { ThemeProvider } from 'mineral-ui/themes';
 import Flex from 'mineral-ui/Flex';
 
+import Header from './components/Header';
 import GitHubCard from './components/GitHubCard';
 
 const App = () => (
   <ThemeProvider>
     <StrictMode>
       <Flex direction="column" alignItems="center">
+        <Header />
         <GitHubCard />
       </Flex>
     </StrictMode>
