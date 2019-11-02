f02ff13701d81fd8522503156b5ff57bae48f7fb
Westley Feller <feller.westley@gmail.com>
2019-11-02 08:42:10 -0700

    Card component created

---
+++ react-github-user-card/src/components/GithubCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class GithubCard extends React.Component {
+    render() {
+        return (
+            <p>yo watup</p>
+        )
+    }
+}
+
+export default GithubCard;
